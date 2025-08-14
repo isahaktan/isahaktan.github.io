ae8c183386bb8e1d794c3a3764e941cee21818d6
İsa Haktan <isahaktanclk@hotmail.com>
2025-08-14 04:56:27 +0300

    dt
    dt

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("Dark").addEventListener("click", function () {
+
+
+    document.getElementById("Dark").style.display = "none";
+    document.getElementById("Music").play();
+
+})
+
